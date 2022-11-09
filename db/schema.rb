f2f898a81a163230789aea9d995aaa7e7c33639f
abigailbarreto <abigailbarreto1998@gmail.com>
2022-11-09 14:57:59 -0300

    migraciones corregidas

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_11_04_191341) do
+ActiveRecord::Schema.define(version: 2022_11_08_173834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.text "description"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -27,19 +42,5 @@ ActiveRecord::Schema.define(version: 2022_11_04_191341) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-  end 
-
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.float "price"
-    t.text "description"
-    t.bigint "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_products_on_category_id"
-  end
-
-  add_foreign_key "products", "categories", "users"
+  add_foreign_key "products", "categories"
 end
