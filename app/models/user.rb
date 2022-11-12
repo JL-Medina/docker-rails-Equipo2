df6db5f91b9c06add7e2a2bf14095b9e11e506fb
nicobritto <nicobritto21@gmail.com>
2022-11-11 22:05:44 -0300

    validacion de campos user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+
+
+
+  validates :name , :lastname, :email,   presence: true
+  validates :email  , uniqueness: {case_insensitive: false}
+  validate :due_date_validity
+
+  def due_date_validity
+    return if fecha_nac.blank?
+    return if fecha_nac < Date.today
+    errors.add :fecha_nac 
+  end
 end
