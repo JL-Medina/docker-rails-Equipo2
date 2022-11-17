5e8cdb102512fbdf06efdc5ce4d638a88672c53f
abigailbarreto <abigailbarreto1998@gmail.com>
2022-11-17 15:47:06 -0300

    vistas devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,7 @@ class User < ApplicationRecord
   #validates :name , :lastname, :email,   presence: true
   validates :email  , uniqueness: {case_insensitive: false}
   validate :due_date_validity
-
-  def is_a_valid_email?(email)
-    (email =~ /^(([A-Za-z0-9]*\.+*_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\+)|([A-Za-z0-9]+\+))*[A-Z‌​a-z0-9]+@{1}((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,4}$/i)
-  end
+  
   EMAIL_REGEX = /\A
       [^\s@]+ # non-at-sign characters, at least one
       @       # at-sign
@@ -23,10 +20,39 @@ class User < ApplicationRecord
       [^\s.@] # non-at-sign and non-period character
       \z/x
 
+  def is_a_valid_email?(email)
+    if (email = EMAIL_REGEX)
+      return true 
+    end 
+  end
 
-  def due_date_validity
+  def birth_date_validity
     return if fecha_nac.blank?
     return if fecha_nac < Date.today
     errors.add :fecha_nac 
+  end
+  
+  def authenticate_user!(opts={})
+    if user_signed_in?
+      super 
+    else
+      if controller_name == "invitations" && (action_name == "edit" || action_name == "update" )
+        super
+      else
+        if params[:red].present?
+          if is_ignored_back_url_action?
+            super
+          else
+            redirect_to new_user_session_path(red: params[:red],back_url: back_url)
+          end
+        else
+          if is_ignored_back_url_action?
+            super
+          else
+            redirect_to new_user_session_path(back_url: back_url)
+          end
+        end
+      end
+    end
   end
 end
