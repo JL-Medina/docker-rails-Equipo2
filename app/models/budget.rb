054dc46303706d025b334d072b0be8c4d4c044de
abigailbarreto <abigailbarreto1998@gmail.com>
2022-12-14 21:50:44 -0300

    suma del precio total del budget y su cantidad de productos

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,5 +1,14 @@
 class Budget < ApplicationRecord
   belongs_to :user
+  before_save :total_budget, :quantity_budget 
+
+  def total_budget
+    self.total_budget =  LineItem.where(budget_id: self.id).sum(:totalprice)
+  end
+
+  def quantity_budget
+    self.quantity_products =  LineItem.where(budget_id: self.id).sum(:quantity)
+  end
 
   validates :name, presence: true
   validates :name, uniqueness: true
